13d5de9020c566d756888af59473e39efcf129de
misschootemup <misschootemup@gmail.com>
2024-02-28 10:51:45 -0600

    added type defs based on models

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const typeDefs = `
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input BookData {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: BookData!): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
